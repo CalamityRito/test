44380ed0287cdaa1acd8ef00e329e53ae58e85fb
Ze Baldwin <zebaldwin08@gmail.com>
2024-12-18 12:12:31 -0700

    defined "randomword" function

---
+++ wordle.py
@@ -0,0 +1,5 @@
+import random
+def randomword():
+    listofwords=['angel','angle','begin','being','binge','stops','spots','posts','traps','strap','parts','cares','carts','space','scale','laces']
+    word=random.listofwords
+    return(word)
