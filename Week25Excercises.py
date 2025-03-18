3bd30938389368fc381948d68ece5e683894c924
Ze Baldwin <zebaldwin08@gmail.com>
2025-03-18 15:27:19 -0700

    Finished both Excercises for week 25

--- Week25Excercises.py
+++ Week25Excercises.py
@@ -9,3 +9,19 @@ with open('mbox-short.txt') as h:
         if len(x) > 0:
             count+=1
 print('mbox-short.txt had',count,'lines that matched',y)
+u=input('Enter file name:')
+average=0
+count2=0
+try:
+    with open(u) as v:
+        for line2 in v:
+            if len(line2)==0: continue
+            line2=line2.strip()
+            j=re.findall('^New Revision: ([0-9]+)',line2)
+            if len(j)>0:
+                count2+=1
+                t=int(j[0])
+                average+=t
+except:
+    print("ERROR")
+print(average/count2)
