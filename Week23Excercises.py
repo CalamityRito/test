c21ab81db243d8acd73ce03f664a0171227e196a
Ze Baldwin <zebaldwin08@gmail.com>
2025-03-20 15:38:59 -0700

    Week 23 Excercises 2 & 3: Find most common hour an email was sent, and find the most common letter in the phrase. completed

--- Week23Excercises.py
+++ Week23Excercises.py
@@ -1,5 +1,6 @@
 a=input('Enter file name:')
 d=dict()
+d2=dict()
 list1=[]
 mostcommon=('',0)
 try:
@@ -19,3 +20,34 @@ try:
         print(item)
 except:
     print('Error')
+list2=[]
+try:
+    with open(a) as h:
+        for line in h:
+            if line.startswith('From '):
+                i=line.split()
+                p=i[5].split(':')
+                d2[p[0]]=d2.get(p[0],0)+1
+        for item in d2:
+            list2.append((item,d2[item]))
+        list2.sort()
+        for t,j in list2:
+            print(t,j)
+except:
+    print('nope')
+d3=dict()
+mostfrequent=['',0]
+try:
+    with open(a) as h:
+        for line in h:
+            for _ in line:
+                d3[_]=d3.get(_,0)+1
+    for item in d3:
+        item=str(item)
+        if item.isalpha():
+            if int(d3[item])>mostfrequent[1]:
+                mostfrequent=[item,d3[item]]
+    for item in mostfrequent:
+        print(item)
+except:
+    print('Get your OWN name!')
