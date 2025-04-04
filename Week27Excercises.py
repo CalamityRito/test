b889a614ebb9824a290f6e37995826f5204303f7
Ze Baldwin <zebaldwin08@gmail.com>
2025-04-04 09:18:26 -0700

    Excercises 3 & 4 for week 27

--- Week27Excercises.py
+++ Week27Excercises.py
@@ -1,18 +1,23 @@
-#Excercise 1:
-#Basic Class Definition
+#Excercises 1&3:
+#Basic Class Definition and Adding Attributes
 class Animal:
-    def __init__(self):
-        pass
+    def __init__(self,att):
+        self.att=att
     def make_sound(self):
         print('woof woof')
-tim=Animal()
+tim=Animal('three two one')
 tim.make_sound()
-#Excercise 2:
-#Inheritance
+#Excercises 2&3:
+#Inheritance and Using Attributes
 class Dog(Animal):
-    def __init__(self):
-        pass
     def make_sound(self):
-        print('bark bark')
-john=Dog()
+        print(self.att)
+john=Dog('master')
 john.make_sound()
+#Excercise 4:
+#Multiple Subclasses
+class Cat(Animal):
+    def make_sound(self):
+        print(self.att,'sucks')
+mary=Cat('Mary')
+mary.make_sound()
